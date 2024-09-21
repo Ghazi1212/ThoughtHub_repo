ff78d0a51599718c3634f903a64cd7185401f4da
Ghazi1212 <120700277+Ghazi1212@users.noreply.github.com>
2024-09-21 04:32:01 -0700

    Update main.jsx
    updated the client Id to prevent attack

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="98657087874-gf3midcij9oh1gj9scdrmjrcac4pl17o.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="">
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
